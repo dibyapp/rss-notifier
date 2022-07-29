ae49d987c58fd22a4d8dc2df0750a7fe19ef7132
dibyapp <dibyapp@outlook.com>
2022-07-29 20:36:16 +0530

    windows update

--- src/Notifier.java
+++ src/Notifier.java
@@ -4,28 +4,36 @@ import java.io.InputStreamReader;
 public class Notifier {
 
 	public static void main(String[] args) {
-		try{
-            String cmd = "";
-            if(System.getProperty("os.name").startsWith("Windows")) {   
-                    cmd = "ping -n 1 ";
-            } else {
-                    cmd = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep CtlRSSI;";
-            }
+		try {
+			String cmd = "";
+			if (System.getProperty("os.name").startsWith("Windows")) {
+				StringBuffer sb = new StringBuffer();
+				cmd = "netsh wlan show interfaces";
+				Process process = Runtime.getRuntime().exec(cmd);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					sb.append(line);
+				}
+				int per = Integer.parseInt(sb.substring(sb.indexOf("Signal")+25, sb.indexOf("Signal")+33).trim().replace("%", ""));
+				int dBm = (per/2)-100;
+				System.err.println(dBm);
+			} else {
+				cmd = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep CtlRSSI;";
+				Process myProcess = Runtime.getRuntime().exec(cmd);
+				myProcess.waitFor();
+				if (myProcess.exitValue() == 0) {
+					BufferedReader output = new BufferedReader(new InputStreamReader(myProcess.getInputStream()));
+					System.err.println("ok");
+					System.err.println(output.readLine());
+				} else {
+					System.err.println(" not ok");
+				}
+			}
 
-            Process myProcess = Runtime.getRuntime().exec(cmd);
-            myProcess.waitFor();
-
-            if(myProcess.exitValue() == 0) {
-            	BufferedReader output = new BufferedReader(new InputStreamReader(myProcess.getInputStream()));
-            	System.err.println(output.readLine());
-            } else {
-            	System.err.println(" not ok");
-            }
-
-    } catch( Exception e ) {
-            e.printStackTrace();
-            System.err.println(" not ok");
-    }
+		} catch (Exception e) {
+			System.err.println("err-askdib-code broken");
+		}
 	}
-	
+
 }
